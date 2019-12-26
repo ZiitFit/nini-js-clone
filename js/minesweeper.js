95d1c63dff91e22297a9185b0a53f3bb67c8d524
nini <nini@marketdesigners.com>
2019-12-27 01:54:59 +0900

    지뢰찾기 완성

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -2,9 +2,19 @@
 var tbody = document.querySelector('table tbody');
 var stopPlay = false;
 var opendNum = 0;
+var dictionary = {
+    opened: -1,
+    question: -2,
+    flag: -3,
+    flagmine: -4,
+    questionmine: -5,
+    mine: 1,
+    default: 0
+}
 
 document.querySelector('#play').addEventListener('click',function(){
     tbody.innerHTML = '';
+    stopPlay = false;
     document.querySelector('#result').textContent = '' ;
     var colNum = document.querySelector('#cols').value;
     var rowNum = document.querySelector('#rows').value;
@@ -17,7 +27,7 @@ document.querySelector('#play').addEventListener('click',function(){
 
     // 지뢰 설정
     var shuffle = [];
-    while(array.length > 80){
+    while(array.length > colNum * rowNum - mineNum){
         shuffle.push(array.splice(Math.floor(Math.random()*array.length),1)[0]);
     }
 
@@ -30,7 +40,7 @@ document.querySelector('#play').addEventListener('click',function(){
         for (var j = 0; j < colNum; j++) {
             var td = document.createElement('td');
             tr.appendChild(td);
-            dataset[i].push(0);
+            dataset[i].push(dictionary.default); //0
             td.addEventListener('contextmenu',function(e){
                 e.preventDefault();
                 var tr = e.currentTarget.parentNode
@@ -38,14 +48,30 @@ document.querySelector('#play').addEventListener('click',function(){
                 var col = Array.prototype.indexOf.call(tr.children,e.currentTarget);
                 var row = Array.prototype.indexOf.call(tbody.children,tr);
                 if(e.currentTarget.textContent === '' || e.currentTarget.textContent === 'X'){
-                    e.currentTarget.textContent = '!';
+                    e.currentTarget.textContent = '!'; 
+                    e.currentTarget.classList.add('flag');
+                    if(dataset[row][col] === dictionary.mine){
+                        dataset[row][col] = dictionary.flagmine
+                    } else {
+                        dataset[row][col] = dictionary.flag
+                    }
                 } else if(e.currentTarget.textContent === '!'){
                     e.currentTarget.textContent = '?';
+                    e.currentTarget.classList.add('question');
+                    if(dataset[row][col] === dictionary.flagmine){
+                        dataset[row][col] = dictionary.questionmine
+                    } else {
+                        dataset[row][col] = dictionary.question
+                    }
                 } else if(e.currentTarget.textContent === '?'){
-                    if(dataset[row][col] === 1){
-                        e.currentTarget.textContent = '';
-                    } else{
+                    e.currentTarget.classList.remove('flag');
+                    e.currentTarget.classList.remove('question');
+                    if(dataset[row][col] === dictionary.questionmine){
                         e.currentTarget.textContent = 'X';
+                        dataset[row][col] = dictionary.mine
+                    } else{
+                        e.currentTarget.textContent = '';
+                        dataset[row][col] = dictionary.default
                     }
                 }
             });
@@ -58,7 +84,7 @@ document.querySelector('#play').addEventListener('click',function(){
                 var col = Array.prototype.indexOf.call(tr.children,e.currentTarget);
                 var row = Array.prototype.indexOf.call(tbody.children,tr);
 
-                if(dataset[row][col] == 1){
+                if([dictionary.opened,dictionary.flag,dictionary.flagmine,dictionary.questionmine,dictionary.question].includes(dataset[row][col])){
                     return;
                 }
 
@@ -71,11 +97,11 @@ document.querySelector('#play').addEventListener('click',function(){
                     document.querySelector('#result').textContent = '실패' ;
                 } else {
                     var mineSearchNum = 0;
-                    dataset[row][col] = 1;
+                    dataset[row][col] = dictionary.opened;
                     for (var i = -1; i < 2; i++) {
                         for (var j = -1; j < 2; j++) {
-                            if(row+i !== -1 && row+i !== 10 && col+j !== -1 && col+j !== 10){
-                                if(dataset[row+i][col+j] == 'X'){
+                            if(row+i > -1 && row+i < rowNum && col+j > -1 && col+j < colNum){
+                                if(dataset[row+i][col+j] == dictionary.mine){
                                     mineSearchNum+=1;
                                 }
                             } 
@@ -85,8 +111,8 @@ document.querySelector('#play').addEventListener('click',function(){
                     if(mineSearchNum === 0){
                         for (var i = -1; i < 2; i++) {
                             for (var j = -1; j < 2; j++) {
-                                if(row+i !== -1 && row+i !== 10 && col+j !== -1 && col+j !== 10){
-                                    if(dataset[row+i][col+j] !== 'X' || dataset[row+i][col+j] !== 1){
+                                if(row+i > -1 && row+i < rowNum && col+j > -1 && col+j < colNum){
+                                    if(dataset[row+i][col+j] !== dictionary.mine || dataset[row+i][col+j] !== dictionary.opened){
                                         tbody.children[row+i].children[col+j].click();
                                     }
                                 } 
@@ -104,9 +130,9 @@ document.querySelector('#play').addEventListener('click',function(){
     
     // 지뢰 심기 
     for (var i = 0; i < shuffle.length; i++) {
-        var row = Math.floor(shuffle[i]/10);
-        var cell = shuffle[i]%10;
-        tbody.children[cell].children[row].textContent = 'X';
-        dataset[cell][row] = 'X';
+        var x = Math.floor(shuffle[i]/10);
+        var y = shuffle[i]%10;
+        tbody.children[y].children[x].textContent = 'X';
+        dataset[y][x] = dictionary.mine;
     }
 });
