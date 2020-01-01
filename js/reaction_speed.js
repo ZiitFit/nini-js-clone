3da6244133fe9fe7d3a1126c4b26edcaba6a4721
nini <nini@marketdesigners.com>
2020-01-02 01:42:21 +0900

    반응속도 테스트 끝

--- js/reaction_speed.js
+++ js/reaction_speed.js
@@ -1,7 +1,5 @@
 var screen = document.querySelector('#screen');
 
-var status = {
-};
 /* 시간 측정 방법
 1. 일반적 사용
     var startTime = new Date();
@@ -14,25 +12,43 @@ var status = {
     var startTime = performance.now();
     var endTime = performance.now();
 */
+
+/* 호출스택
+후입선출 구조
+먼저 들어간게 제일 나중에 나옴, 제일 나중에 들어간 게 제일 먼저 나옴
+*/
+
+var startTime,endTime;
+var score = [];
+var timeout;
+
 screen.addEventListener('click',function(){
-    var startTime;
     if(screen.classList.contains('waiting')){
         screen.classList.remove('waiting');
         screen.classList.add('ready');
         screen.textContent = '초록색이 되면 클릭하세요';
-        setTimeout(function(){
+        timeout = setTimeout(function(){
             startTime = new Date();
             screen.click();
         },Math.random()*1000+2000); //2000~3000사이 수
     } else if(screen.classList.contains('ready')){
-        screen.classList.remove('ready');
-        screen.classList.add('now');
-        screen.textContent = '클릭하세요';
+        if(!startTime){
+            clearTimeout(timeout);
+            screen.classList.remove('ready');
+            screen.classList.add('waiting');
+            screen.textContent = '너무 성급하시군요';
+        } else{
+            screen.classList.remove('ready');
+            screen.classList.add('now');
+            screen.textContent = '클릭하세요';
+        }
     } else if(screen.classList.contains('now')){
-        var endTime = new Date();
-        console.log((endTime-startTime)/1000);
+        endTime = new Date();
+        document.querySelector('#result').textContent = '반응속도:'+(endTime-startTime)/1000+'ms';
+        score.push(endTime-startTime);
+        startTime, endTime = null;
         screen.classList.remove('now');
         screen.classList.add('waiting');
-        screen.textContent = '초록색이 되면 클릭하세요';
+        screen.textContent = '클릭해서 시작하세요';
     }
 });
